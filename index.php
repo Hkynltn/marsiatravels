a960eeb7eb65db6e73b07828b317bca0a84a102d
hkynltn <hiradkeysan@gmail.com>
2024-05-21 11:03:29 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Marsia travels</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+  
+</head>
+<body>
+    <header>
+        <section id="logo-box">
+            <img src="assets/img/logoMarsiaTravels.png" id="logo">
+        <h1>
+            Marsia Travels  
+        </h1>
+        </section>
+    </header>
+        <section class="inleiding">   
+        <div id="search-block">
+            <div class="keuze-blocks">
+
+            </div>
+
+            <div id="search-bar">
+        <p class="best1"></p>
+        <p class="best2"></p>
+        <p class="best3"></p>
+        <img src="assets/img/pinger.png" id="logo2">
+        <img src="assets/img/zoek.png" id="logo3">
+    </div>
+        </section>
+
+
+        <section>
+            <h1 class="welkom">Welkom bij Marsia Travels!</h1>
+            <h2 class="beschrijving">Ben je er klaar voor? Een échte beleving? Die écht lekkere knapperige pizza uit Italië.
+                 Een écht avontuurlijke uitdaging. Die échte rust! Er is niets mis met pizza Hawaii op de bank.
+                 Maar soms is het lekker er écht even helemaal tussenuit te gaan! En daarvoor hebben wij nu
+                 de beste deals. Een échte ervaring, voor de beste prijs. 
+                 Bij Marsia Travels begrijpen we dat reizen meer is dan alleen van A naar B gaan. Het gaat om
+                 het creëren van onvergetelijke herinneringen, het ontdekken van nieuwe culturen en het
+                 beleven van avonturen die je voor altijd bij zullen blijven. Of je nu op zoek bent naar een ontspannende strandvakantie, een avontuurlijke trektocht
+                 door de jungle, of een culturele verkenningstocht door historische steden, wij hebben de
+                 perfecte reis voor jou. Onze ervaren reisadviseurs staan ​​klaar om je te helpen bij het plannen van jouw droomreis.
+                 Met onze uitgebreide kennis van bestemmingen over de hele wereld en ons streven naar
+                 klanttevredenheid, zorgen we ervoor dat jouw reiservaring soepel verloopt van begin tot
+                 eind.
+                 Dus waar wacht je nog op? Grab the deal & make it real! ✈️🌎Boek vandaag nog jouw volgende avontuur met Marsia Travels en maak je klaar voor een
+                 onvergetelijke reiservaring! </h2>
+
+                 <button class="knop">Nu boeken</button>
+                 <img src="assets/img/vliegtuig.png" id="logo4">
+
+        </section>
+
+
+    
+    
+</body>
+</html>
