c4857649f58375b85675d332c0da7a85efcaedd8
MasterHirad <hiradkeysan@gmail.com>
2024-05-29 13:08:13 +0200

    footer + style

--- footer.php
+++ footer.php
@@ -1,33 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Marsia travels</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
 <footer>
-    <div class="buttons-container">
-        <div class="button-column">
-            <button>Klanten service</button>
-            <button>Boek nu</button>
-        </div>
-        <div class="button-column">
-            <button>Contact</button>
-            <button>Volg Marsia Travels</button>
-        </div>
-    </div>
-    <div class="row">
-        <img id="apple" src="assets/img/applepay.png" alt="Applepay_Log">
-        <img id="klarna" src="assets/img/klarna.png" alt="Klarna_Log\">
-        <img id="paypal" src="assets/img/paypal.png" alt="Paypal_Log">
-        <img id="ideal" src="assets/img/ideal.png" alt="Ideal_Log
-    </div>
-    <div class="column">
-        <div>
-            <img src="assets/img/instagram.png" alt="Instagram_Logo">
-            <h2>Instagram</h2>
-        </div>
-        <div>
-            <img src="assets/img/facebook.png" alt="Facebook_Logo">
-            <h2>Facebook</h2>
-        </div>
-        <div>
-            <img src="assets/img/twitter.png" alt="Twitter_Logo">
-            <h2>Twitter</h2>
-        </div>
-    </div>
-</footer>
+       
+    </footer>  
+</body>
 
+   
+
+
+</html>
